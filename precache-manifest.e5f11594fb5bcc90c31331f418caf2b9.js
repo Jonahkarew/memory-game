5c6b56064ace4d008d88fa39c264af179b5b54fc
Jonah <jonahkarew@gmail.com>
2019-05-29 11:23:42 -0400

    Updates

---
+++ precache-manifest.e5f11594fb5bcc90c31331f418caf2b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44504571570787e7eaf2f498e3c0c072",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "a67e6d56f50f00959e42",
+    "url": "/memory-game/static/css/main.28f1cd4d.chunk.css"
+  },
+  {
+    "revision": "ff2b54d40a6d618c8c20",
+    "url": "/memory-game/static/js/2.5996a945.chunk.js"
+  },
+  {
+    "revision": "a67e6d56f50f00959e42",
+    "url": "/memory-game/static/js/main.bb9dbbc7.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
